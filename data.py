236d94e9858cf96d3dcea6ca08c2ef1c29bff4fe
Reddonut-cyber <akhowsuk@gmail.com>
2025-02-01 18:28:27 +0700

    Add database connection and create tables for Students and Phone_numbers

--- data.py
+++ data.py
@@ -1,8 +1,35 @@
 import sqlite3
-import os
 
-db_folder = ""
-if not os.path.exists(db_folder):
-    os.makedirs(db_folder)
+conn = sqlite3.connect('Inclass.db')
+c = conn.cursor()
 
-con = sqlite3.connect(f"{db_folder}/demodb.db")
+# c.execute("""
+#           CREATE TABLE IF NOT EXISTS Students (
+#               id INTEGER PRIMARY KEY,
+#               name TEXT NOT NULL,
+#               age INTEGER NOT NULL,
+#               grade INTEGER NOT NULL
+#           )""")
+
+# c.execute("""CREATE TABLE IF NOT EXISTS Phon_numbers (
+#             student_id INTEGER,
+#             number TEXT NOT NULL,
+#             FOREIGN KEY (student_id) REFERENCES Students(id)
+#             )""")
+
+# c.executemany("INSERT INTO Students (name, age, grade) VALUES (?,?,?)", [
+#     ('John Doe', 18, 12),
+#     ('Jane Smith', 17, 11),
+#     ('Bob Johnson', 19, 10)
+#     ])
+
+# c.executemany("INSERT INTO Phon_numbers (student_id, number) VALUES (?,?)", [
+#     (1, '123-456-7890'),
+#     (2, '987-654-3210'),
+#     (3, '555-555-5555')
+#     ])
+
+
+
+conn.commit()
+conn.close()
